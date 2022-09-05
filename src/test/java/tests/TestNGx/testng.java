745cda5dfd30b79682ffdeb17f745c84907f83ca
Enes Taşyürek <tasyurekenes22@gmail.com>
2022-09-05 21:39:19 +0300

    TestNG

--- src/test/java/tests/TestNGx/testng.java
+++ src/test/java/tests/TestNGx/testng.java
@@ -1,11 +1,14 @@
 package tests.TestNGx;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class testng {
 
     @Test
     public void test1(){
-        System.out.println("test1");
+        String str = "TestNG is working fine";
+        String str2 = "TestNG is working fine";
+        Assert.assertEquals(str,str2);
     }
 }
