0b06c80f44784a9a82c3199a8bc6d7baa62b7f95
Enes Taşyürek <tasyurekenes22@gmail.com>
2022-09-06 17:43:53 +0300

    Task5_5

---
+++ src/test/java/tests/Tasks/Task5_5.java
@@ -0,0 +1,50 @@
+package tests.Tasks;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Task5_5 {
+    WebDriver driver;
+
+    //TC #5: Selecting state from State dropdown and verifying result
+    //1. Open Chrome browser
+    //2. Go to http://practice.cybertekschool.com/dropdown
+    @BeforeMethod
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/dropdown");
+    }
+
+    //3. Select Illinois
+    //4. Select Virginia
+    //5. Select California
+    //6. Verify final selected option is California.
+    //Use all Select options. (visible text, value, index)
+    @Test
+    public void test() throws InterruptedException {
+        Select stateDropdown = new Select(driver.findElement(By.id("state")));
+        stateDropdown.selectByVisibleText("Illinois");
+        stateDropdown.selectByValue("VA");
+        stateDropdown.selectByIndex(5);
+        Assert.assertEquals(stateDropdown.getFirstSelectedOption().getText(), "California");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+    }
+
+
+}
